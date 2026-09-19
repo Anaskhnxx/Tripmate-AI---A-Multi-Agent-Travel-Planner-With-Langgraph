885d2d3d7e33a02f6f7d9b015be3795176efd8ed
anaskhan <anaskhan48589@gmail.com>
2026-09-19 13:18:08 +0530

    port add

--- app.py
+++ app.py
@@ -107,9 +107,11 @@ async def favicon():
 
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT",8000))
     uvicorn.run(
         "app:app",
-        host="127.0.0.1",
-        port=8000,
+        host="0.0.0.0",
+        port=port,
         reload=True
     )
